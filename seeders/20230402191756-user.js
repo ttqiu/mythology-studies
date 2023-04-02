8833913c081a09f6ddaa5b73db8dc28220597f7a
ttqiu <tt.hibakyo.hs7@gmail.com>
2023-04-02 15:21:47 -0400

    user seed

---
+++ seeders/20230402191756-user.js
@@ -0,0 +1,27 @@
+'use strict'
+const middleware = require('../middleware')
+const falso = require('@ngneat/falso')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const users = await Promise.all(
+      [...Array(100)].map(async () => {
+        let password = falso.randPassword()
+        let password_digest = await middleware.hashPassword(password)
+        return {
+          userName: falso.randUserName(),
+          email: falso.randEmail(),
+          passwordDigest: password_digest,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      })
+    )
+    return queryInterface.bulkInsert('users', users)
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('users')
+  }
+}
